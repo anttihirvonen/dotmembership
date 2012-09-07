cdb07792b2e83aa47a6b08413f032b4aaf223c0a
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-09-07 18:01:23 +0300

    Added a simple model for member and installed members-app

--- dotmembership/apps/members/models.py
+++ dotmembership/apps/members/models.py
@@ -1,3 +1,18 @@
+# encoding: utf-8
 from django.db import models
 
+
 # Create your models here.
+class Member(models.Model):
+    MEMBERSHIP_CHOICES = ((0, "normaali"),
+                          (1, "kannatus")
+                          (2, "kunnia"))
+    first_name = models.CharField("etunimi", max_length=30)
+    last_name = models.CharField("sukunimi", max_length=30)
+    email = models.EmailField("sähköpostiosoite")
+
+    membership_type = models.IntegerField("jäsentyyppi", choices=MEMBERSHIP_CHOICES)
+
+    class Meta:
+        verbose_name = u"jäsen"
+        verbose_name_plural = u"jäsenet"
