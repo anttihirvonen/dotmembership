5efc66f3fff0883f59dacf2d91bcb017c7363665
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-09-07 18:57:42 +0300

    Translation support added to models

--- dotmembership/apps/billing/models.py
+++ dotmembership/apps/billing/models.py
@@ -2,29 +2,30 @@
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils.translation import ugettext as _
 
 from dotmembership.apps.members.models import Member
 
 
 # Create your models here.
 class Invoice(models.Model):
-    STATUS_CHOICES = ((0, "luotu"),
-                    (1, "lähetetty"),
-                    (2, "maksettu"),
-                    (3, "erääntynyt"),
-                    (4, "poistettu"))
+    STATUS_CHOICES = ((0, _(u"luotu")),
+                    (1, _(u"lähetetty")),
+                    (2, _(u"maksettu")),
+                    (3, _(u"erääntynyt")),
+                    (4, _(u"poistettu")))
     member = models.ForeignKey(Member, related_name="invoice")
 
-    status = models.IntegerField("tila", choices=STATUS_CHOICES)
+    status = models.IntegerField(_(u"tila"), choices=STATUS_CHOICES)
 
     # Dates
-    created = models.DateTimeField(auto_now_add=True, verbose_name=u'luotu')
-    duedate = models.DateField(verbose_name=u'eräpäivä')
+    created = models.DateTimeField(auto_now_add=True, verbose_name=_(u"luotu"))
+    duedate = models.DateField(verbose_name=_(u"eräpäivä"))
 
-    amount = models.DecimalField(max_digits=7, decimal_places=2, verbose_name=u'summa')
+    amount = models.DecimalField(max_digits=7, decimal_places=2, verbose_name=_(u"summa"))
     # Automatically calculated at post_save based on the id
 
-    reference_number = models.IntegerField("viitenumero", blank=True, editable=False)
+    reference_number = models.IntegerField(_(u"viitenumero"), blank=True, editable=False)
 
 
 @receiver(post_save, sender=Invoice)
--- dotmembership/apps/members/models.py
+++ dotmembership/apps/members/models.py
@@ -1,18 +1,19 @@
 # encoding: utf-8
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
 # Create your models here.
 class Member(models.Model):
-    MEMBERSHIP_CHOICES = ((0, "normaali"),
-                          (1, "kannatus"),
-                          (2, "kunnia"))
-    first_name = models.CharField("etunimi", max_length=30)
-    last_name = models.CharField("sukunimi", max_length=30)
-    email = models.EmailField("sähköpostiosoite")
+    MEMBERSHIP_CHOICES = ((0, _(u"normaali")),
+                          (1, _(u"kannatus")),
+                          (2, _(u"kunnia")))
+    first_name = models.CharField(_(u"etunimi"), max_length=30)
+    last_name = models.CharField(_(u"sukunimi"), max_length=30)
+    email = models.EmailField(_(u"sähköpostiosoite"))
 
-    membership_type = models.IntegerField("jäsentyyppi", choices=MEMBERSHIP_CHOICES)
+    membership_type = models.IntegerField(_(u"jäsentyyppi"), choices=MEMBERSHIP_CHOICES)
 
     class Meta:
-        verbose_name = u"jäsen"
-        verbose_name_plural = u"jäsenet"
+        verbose_name = _(u"jäsen")
+        verbose_name_plural = _(u"jäsenet")
