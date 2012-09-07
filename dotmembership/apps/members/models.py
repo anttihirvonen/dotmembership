2fdfe52027e2df4f1924c30b17ca18f8fd0a9f74
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-09-07 20:21:46 +0300

    Several changes to Member-model.
    * Membership converted to use django-model-utils choices
    * Added field for storing the joinin date

--- dotmembership/apps/members/models.py
+++ dotmembership/apps/members/models.py
@@ -2,17 +2,23 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from model_utils import Choices
 
 # Create your models here.
 class Member(models.Model):
-    MEMBERSHIP_CHOICES = ((0, _(u"normaali")),
-                          (1, _(u"kannatus")),
-                          (2, _(u"kunnia")))
+    MEMBERSHIP = Choices(("normal", _(u"normaali")),
+                         ("support", _(u"kannatus")),
+                         ("honorary", _(u"kunnia")))
+
     first_name = models.CharField(_(u"etunimi"), max_length=30)
     last_name = models.CharField(_(u"sukunimi"), max_length=30)
     email = models.EmailField(_(u"sähköpostiosoite"))
 
-    membership_type = models.IntegerField(_(u"jäsentyyppi"), choices=MEMBERSHIP_CHOICES)
+    joined = models.DateTimeField(_(u"liittynyt"), auto_now_add=True, editable=False)
+    membership_type = models.CharField(_(u"jäsentyyppi"), choices=MEMBERSHIP, max_length=15)
+
+    def __unicode__(self):
+        return "{0}, {1} ({2})".format(self.last_name, self.first_name, self.id)
 
     class Meta:
         verbose_name = _(u"jäsen")
