d84630428702a86e48030800f5d210d1660ea48e
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-09-09 18:48:17 +0300

    Implemented e-mail notification on registering a new member.

--- dotmembership/apps/members/forms.py
+++ dotmembership/apps/members/forms.py
@@ -1,4 +1,9 @@
-from django import forms
+# encoding: utf-8
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.sites.models import Site
+from django.conf import settings
 
 from generic_confirmation.forms import DeferredForm
 
@@ -13,3 +18,11 @@ class MemberForm(DeferredForm):
         model = Member
         fields = ("first_name", "last_name", "email", "home_town",
                   "school", "major", "class_year")
+
+    def send_notification(self, user=None, instance=None):
+        subject = _(u"Vahvista liittymisesi DOTin jäseneksi")
+        body = render_to_string("members/mails/confirm_join.txt",
+                                 {'token': instance.token,
+                                  'base_url': "http://{0}".format(Site.objects.get_current().domain),
+                                  'first_name': self.cleaned_data['first_name']})
+        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [self.cleaned_data['email'],])
