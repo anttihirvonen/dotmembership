3edb0c78291236f1cacf967e2512a297c33f763f
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-09-09 17:14:26 +0300

    Simple member join process implemented for testing MemberForm

--- dotmembership/apps/members/views.py
+++ dotmembership/apps/members/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
 
+from .forms import MemberForm
+
+
 def index(request):
     """
     Index page of the member register.
 
     Renders a page which contains forms.
     """
-    return render(request, 'index.html')
+    if request.method == "POST":
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = MemberForm()
+
+    return render(request, 'index.html', {'member_form': form})
+
 
 def join(request):
     """
     AJAX view for adding a new member to registry.
     """
     pass
+
 
 def check_my_data(request):
     pass
