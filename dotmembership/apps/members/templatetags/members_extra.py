0b623ad77ff1331ca30eb603f9362d17b2a2d72a
Antti Hirvonen <antti.hirvonen@gmail.com>
2012-09-13 20:51:28 +0300

    Added templatetag for outputting div-based "table" rows

--- dotmembership/apps/members/templatetags/members_extra.py
+++ dotmembership/apps/members/templatetags/members_extra.py
@@ -1,3 +1,4 @@
+from django.utils.safestring import mark_safe
 from django import template
 register = template.Library()
 
@@ -16,3 +17,8 @@ def getfieldname(model, field):
     Helper to get model field value dynamically.
     """
     return model._meta.get_field(field).verbose_name
+
+
+@register.inclusion_tag('members/div_datarow.html')
+def print_row(th, td, extra_td=""):
+    return {'th': th, 'td': mark_safe(u"%s%s" % (td, extra_td))}
