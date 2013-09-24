cbadb0790c22921aeb20b724dffc815b4bdae973
Antti Hirvonen <antti.hirvonen@gmail.com>
2013-09-24 17:19:19 +0300

    Oops, forgot fee in list_display

--- dotmembership/apps/billing/admin.py
+++ dotmembership/apps/billing/admin.py
@@ -12,7 +12,7 @@ class InvoiceAdmin(reversion.VersionAdmin):
     readonly_fields = ('amount', "reference_number", "for_year", 'member', 'created', 'fee', 'due_date')
     fields = ('member', 'created', 'fee', 'due_date', 'amount', 'reference_number', 'status', 'payment_date',
             'payment_method')
-    list_display = ("member", "status", "reference_number")
+    list_display = ("member", "status", "reference_number", "fee")
     list_filter = ("status", "fee")
     actions = None
 
